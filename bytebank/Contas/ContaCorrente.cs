9ed92694bcf15c923b116e1b8e2af5fa4cff0749
Clauciofds <115019744+Clauciofds@users.noreply.github.com>
2023-03-28 10:47:54 -0300

    Adicionar arquivos de projeto.

---
+++ bytebank/Contas/ContaCorrente.cs
@@ -0,0 +1,80 @@
+﻿using bytebank.Titular;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace bytebank.Contas
+{
+    public class ContaCorrente
+    {
+        public int numero_agencia;
+        public string conta;
+        private double saldo = 100;
+
+        public Cliente titular;
+
+        public void DemostrativoContaCorrente(ContaCorrente nome)
+        {
+            Console.WriteLine($"Titular da conta: {nome.titular.nome}");
+            Console.WriteLine($"Número da conta: {nome.conta}");
+            Console.WriteLine($"Agência: {nome.numero_agencia}");
+            Console.WriteLine($"Saldo R$ {string.Format("{0:0.00}", nome.saldo)}" + "\n" + "\n");
+        }
+        public void Depositar(double valor, ContaCorrente destino)
+        {
+            saldo += valor;
+            Console.WriteLine("Valor do depósito na conte de " + destino.titular + $" R$ {string.Format("{0:0.00}", valor)}");
+            Console.WriteLine($"Saldo após o deposito é: R$ {string.Format("{0:0.00}", destino.saldo)}" + "\n");
+        }
+
+        public void Sacar(double valor, ContaCorrente origem)
+        {
+
+            if (valor <= saldo)
+            {
+                Console.WriteLine("Valor para saque na conta de " + origem.titular + $" é R$ {string.Format("{0:0.00}", valor)}");
+                saldo -= valor;
+                Console.WriteLine("Saldo após último saque na conta de " + origem.titular + $" R$ {string.Format("{0:0.00}", saldo)}" + "\n");
+            }
+            else
+                Console.WriteLine("Saldo insuficiênte");
+        }
+
+        public void Transferir(double valor, ContaCorrente origem, ContaCorrente destino)
+        {
+            if (saldo >= valor)
+            {
+                Sacar(valor, origem);
+                destino.Depositar(valor, destino);
+
+                Console.WriteLine("Valor da transferência na conta de " + origem.titular + $" é R$ {string.Format("{0:0.00}", valor)}");
+                Console.WriteLine("Saldo após transfência " + destino.titular + $" R$ {string.Format("{0:0.00}", destino.saldo)}" + "\n");
+            }
+            else
+            {
+                Console.WriteLine("\n" + "O saldo de " + origem.titular + " insuficênte para transferência");
+            }
+        }
+
+        public void SetSaldo(double valor)
+        {
+            if (valor < 0)
+            {
+                return;
+            }
+            else
+            {
+                this.saldo = valor;
+            }
+        }
+
+        public double GetSaldo()
+        {
+            return this.saldo;
+        }
+    }
+}
---
+++ bytebank/Program.cs
@@ -0,0 +1,85 @@
+﻿using bytebank.Contas;
+using bytebank.Titular;
+
+
+
+//criando conta corrente andré
+//ContaCorrente contaAndre = new ContaCorrente();
+//contaAndre.titular = "André silva";
+//contaAndre.numero_agencia = 15;
+//contaAndre.conta = "1010-x";
+//contaAndre.saldo = 1000;
+
+
+//contaAndre.DemostrativoContaCorrente(contaAndre);
+
+
+
+//// CRIANDO CONTA CORRENTE MARIA
+//ContaCorrente contaMaria = new ContaCorrente();
+//contaMaria.titular = "Maria Souza";
+//contaMaria.numero_agencia=17;
+//contaMaria.conta = "1040-G";
+//contaMaria.saldo = 500;
+
+//contaMaria.DemostrativoContaCorrente(contaMaria);
+
+
+
+//// MOVIMENTAÇÕES E EXTRATO
+//contaMaria.Sacar(50, contaMaria);
+
+//contaMaria.Transferir(100, contaMaria, contaAndre);
+
+
+
+//// CRIANDO CONTA PEDRO DE FORMA PARCIAL E OBSERVANDO OS RETORNO DOS PADRÕES CRIANDO NA CLASSE  ContaCorrente
+//ContaCorrente contaPedro = new ContaCorrente();
+//contaPedro.titular = "Pedro Silva";
+
+//contaPedro.DemostrativoContaCorrente(contaPedro);
+
+//double valor = 300;
+//double valor2 = valor;
+
+//Console.WriteLine(valor==valor2);
+//Console.WriteLine(valor);
+//Console.WriteLine(valor2)
+
+
+//Cliente cliente = new Cliente();
+//cliente.nome = "Andre silva";
+//cliente.cpf = "123456789";
+//cliente.profissao = "Analista";
+
+//ContaCorrente conta = new ContaCorrente();
+//conta.titular = cliente;
+//conta.conta = "1010-x";
+//conta.numero_agencia = 15;
+//conta.saldo = 1000;
+
+//conta.DemostrativoContaCorrente(conta);
+
+//conta.Sacar(45, conta);
+
+
+//ContaCorrente conta2 = new ContaCorrente();
+//conta2.titular = new Cliente();
+//conta2.titular.nome = "José souza";
+//conta2.titular.profissao = "tester";
+//conta2.titular.cpf = "987654321";
+//conta2.conta = "9999-x";
+//conta2.numero_agencia = 18;
+//conta2.saldo = 500;
+
+//conta2.DemostrativoContaCorrente(conta2);
+
+ContaCorrente conta3 = new ContaCorrente();
+conta3.SetSaldo(250);
+
+Console.WriteLine(conta3.GetSaldo());
+
+
+//fim da execução
+Console.WriteLine("\n" + "Tecle Enter para fechar..");
+Console.ReadLine();
